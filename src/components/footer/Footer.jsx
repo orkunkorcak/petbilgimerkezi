650445eb369b5b6ced26c956afef4b5ae8fe70b0
your_name <orkunkorcak@gmail.com>
2025-09-24 19:23:37 +0300

    footer düzenlendi

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,23 @@ import css from "./Footer.module.css";
 const Footer = () => {
     return (
       <footer className={css.footer}>
-        <p>&copy; 2025 Developer By Orkun Korçak</p>
-        </footer>
-    
+        <p>
+          Bu sitedeki içerikler sadece bilgilendirme amaçlıdır ve bir uzmana
+          danışılmalıdır.
+        </p>
+        <p>
+          İletişim için{" "}
+          <a href="mailto:petbilgimerkezi@gmail.com">
+            petbilgimerkezi@gmail.com
+          </a>
+        </p>
+        <p>
+          &copy; 2025 Developer{" "}
+            <a href="https://www.linkedin.com/in/orkun-kor%C3%A7ak-81a096321/">
+              Orkun Korçak
+          </a>
+        </p>
+      </footer>
     );
 }
     
